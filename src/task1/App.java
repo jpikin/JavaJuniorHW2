92fbe75a010dee8d6d6c5c8a0fe8c1b7d5c98ade
pikin <pikin@bk.ru>
2024-03-31 22:40:19 +0500

    task1 fixed

--- src/task1/App.java
+++ src/task1/App.java
@@ -6,7 +6,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
 
 /*
